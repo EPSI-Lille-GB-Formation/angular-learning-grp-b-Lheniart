e80858eaa75abf94456ba96e4b932a37199ebe62
lheniart <lheniart@be-infotech.fr>
2023-12-22 08:38:12 +0100

    First push

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {TODOS} from "./mock-todo";
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: ` <h1>Hello World !</h1> `,
+  template: `
+    <div class="container">
+      <h1>Liste de chose à faire</h1>
+      <ul>
+        <li *ngFor="let todo of IncomleteTodos">
+            {{todo.title}}
+        </li>
+      </ul>
+    </div>
+  `,
   styles: [],
 })
 export class AppComponent {
-  title = 'todolist';
+  todoList  = TODOS
+  constructor(){
+    console.table(this.todoList);
+  }
+  selectTodo(id: number):Todo {
+    return this.todoList[id]
+  }
+  get IncomleteTodos() {
+    return this.todoList.filter(todo => !todo.isCompleted);
+  }
 }
